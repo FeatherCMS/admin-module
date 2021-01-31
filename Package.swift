2cf820c9b8e7a10675e95601bccc8f36c8fb46a6
Tibor Bödecs <mail.tib@gmail.com>
2021-01-31 20:09:32 +0100

    Dep updates, Peacock CSS, admin fixes [wip]

--- Package.swift
+++ Package.swift
@@ -1,6 +1,56 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let isLocalTestMode = false
+
+var deps: [Package.Dependency] = [
+    .package(url: "https://github.com/FeatherCMS/feather-core", from: "1.0.0-beta"),
+]
+
+var targets: [Target] = [
+    .target(name: "AdminModule", dependencies: [
+        .product(name: "FeatherCore", package: "feather-core"),
+    ],
+    resources: [
+        .copy("Bundle"),
+    ]),
+]
+
+// @NOTE: https://bugs.swift.org/browse/SR-8658
+if isLocalTestMode {
+    deps.append(contentsOf: [
+        /// drivers
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
+        /// core modules
+        .package(url: "https://github.com/FeatherCMS/user-module", from: "1.0.0-beta"),
+        .package(url: "https://github.com/FeatherCMS/api-module", from: "1.0.0-beta"),
+        .package(url: "https://github.com/FeatherCMS/system-module", from: "1.0.0-beta"),
+        .package(url: "https://github.com/FeatherCMS/frontend-module", from: "1.0.0-beta"),
+        .package(url: "https://github.com/FeatherCMS/blog-module", from: "1.0.0-beta"),
+    ])
+    targets.append(contentsOf: [
+        .target(name: "Feather", dependencies: [
+            .product(name: "FeatherCore", package: "feather-core"),
+
+            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
+
+            /// core modules
+            .product(name: "UserModule", package: "user-module"),
+            .product(name: "ApiModule", package: "api-module"),
+            .product(name: "SystemModule", package: "system-module"),
+            .product(name: "FrontendModule", package: "frontend-module"),
+            .product(name: "BlogModule", package: "blog-module"),
+
+            .target(name: "AdminModule"),
+        ]),
+        .testTarget(name: "AdminModuleTests", dependencies: [
+            .target(name: "AdminModule"),
+        ])
+    ])
+}
+
 let package = Package(
     name: "admin-module",
     platforms: [
@@ -9,31 +59,6 @@ let package = Package(
     products: [
         .library(name: "AdminModule", targets: ["AdminModule"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/binarybirds/feather-core", from: "1.0.0-beta"),
-
-        /// drivers
-//        .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
-//        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
-    ],
-    targets: [
-        .target(name: "AdminModule", dependencies: [
-            .product(name: "FeatherCore", package: "feather-core"),
-        ],
-        resources: [
-            .copy("Bundle"),
-        ]),
-//        @NOTE: https://bugs.swift.org/browse/SR-8658
-//        .target(name: "Feather", dependencies: [
-//            .product(name: "FeatherCore", package: "feather-core"),
-//
-//            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-//            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
-//
-//            .target(name: "AdminModule"),
-//        ]),
-        .testTarget(name: "AdminModuleTests", dependencies: [
-            .target(name: "AdminModule"),
-        ])
-    ]
+    dependencies: deps,
+    targets: targets
 )
--- Sources/Feather/main.swift
+++ Sources/Feather/main.swift
@@ -6,18 +6,42 @@
 //
 
 import FeatherCore
-import FluentSQLiteDriver
-import LiquidLocalDriver
+
+import SystemModule
+import UserModule
+import ApiModule
+import FrontendModule
+import BlogModule
 import AdminModule
+
+/// setup metadata delegate object
+Feather.metadataDelegate = FrontendMetadataDelegate()
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let feather = try Feather(env: env)
 defer { feather.stop() }
 
-try feather.configure(database: .sqlite(.file("db.sqlite")),
-                      databaseId: .sqlite,
-                      fileStorage: .local(publicUrl: Application.baseUrl, publicPath: Application.Paths.public, workDirectory: "assets"),
-                      fileStorageId: .local,
-                      modules: [AdminBuilder()])
+feather.useSQLiteDatabase()
+feather.useLocalFileStorage()
+feather.usePublicFileMiddleware()
+feather.setMaxUploadSize("10mb")
+
+try feather.configure([
+    /// core
+    SystemBuilder(),
+    UserBuilder(),
+    ApiBuilder(),
+    AdminBuilder(),
+    FrontendBuilder(),
+    /// other
+    BlogBuilder(),
+])
+
+
+/// reset resources folder if we're in debug mode
+if feather.app.isDebug {
+    try feather.reset(resourcesOnly: false)
+}
+
 try feather.start()
